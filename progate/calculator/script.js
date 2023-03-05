fe7506831700ee3d743faa906fe3cefd4800b316
ArdhaIrfan <ardhairfan17@gmail.com>
2023-03-05 13:12:58 +0700

    Add calculator

---
+++ progate/calculator/script.js
@@ -0,0 +1,40 @@
+let display = document.getElementById('display');
+let button = Array.from(document.getElementsByTagName('button'));
+let equal = 0;
+
+button.map(button => {
+    button.addEventListener('click', (e) => {
+        console.log('clicked');
+        console.log(e);
+        console.log(e.target);
+        console.log(e.target.innerText);
+        console.log(e.target.innerHTML);
+        if(equal == 1) {
+            display.innerText = '';
+            equal = 0;
+        }
+        switch(e.target.innerHTML) {
+            case 'C':
+                display.innerText = '';
+                break;
+            case '<i class="fa-solid fa-delete-left"></i>':
+                if(display.innerText) {
+                    display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            case '×':
+                display.innerText += '*';
+                break;
+            case '=':
+                try {
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = 'Error';
+                }
+                equal = 1;
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
