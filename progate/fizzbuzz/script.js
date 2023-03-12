54bd3624db8a4c24f4227c9b4a5473839a645599
ArdhaIrfan <ardhairfan17@gmail.com>
2023-03-13 00:18:24 +0700

    FizzBuzz done

--- progate/fizzbuzz/script.js
+++ progate/fizzbuzz/script.js
@@ -1,5 +1,6 @@
 let button = document.getElementById('button');
 let parent = document.getElementsByClassName('display')[0];
+let word = document.getElementById('word');
 
 button.addEventListener('click', (e) => {
     e.preventDefault();
@@ -12,16 +13,27 @@ button.addEventListener('click', (e) => {
     if (number > 0) {
         if (number%3 == 0 && number%5 == 0) {
             child.innerText = "FizzBuzz!";
+            child.classList.add('violet');
             parent.appendChild(child);
+            word.innerText = 'FizzBuzz!';
+            word.style.color = 'rgb(162, 93, 225)';
         } else if (number%5 == 0) {
             child.innerText = "Buzz!";
+            child.classList.add('blue');
             parent.appendChild(child);
+            word.innerText = 'Buzz!';
+            word.style.color = 'rgb(44, 163, 214)';
         } else if (number%3 == 0) {
             child.innerText = "Fizz!";
+            child.classList.add('red');
             parent.appendChild(child);
+            word.innerText = 'Fizz!';
+            word.style.color = 'rgb(231, 67, 67)';
         } else {
             child.innerText = number;
             parent.appendChild(child);
+            word.innerText = number;
+            word.style.color = 'white';
         }
     } else {
         child.innerText = "Not a valid number";
