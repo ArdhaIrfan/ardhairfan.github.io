1c2e6f0610f8bb99efb9d70371ff5ae9c33098b7
ArdhaIrfan <ardhairfan17@gmail.com>
2023-03-19 12:14:39 +0700

    Add Form Validation

--- progate/script.js
+++ progate/script.js
@@ -71,3 +71,61 @@ document.querySelectorAll('.project img').forEach(image => {
 document.querySelector('.modal span').onclick = () => {
     document.querySelector('.modal').classList.remove('active');
 }
+
+// ================================= Form Validation =================================
+
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+    console.log(inputControl);
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+}
+
+const setSuccess = (element) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+    console.log(inputControl);
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+}
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const emailValue = email.value.trim();
+    const messageValue = message.value;
+
+    if (emailValue === '') {
+        setError(email, "Email is Required");
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, "Email format is invalid");
+    } else {
+        setSuccess(email);
+    }
+
+    if (messageValue === '') {
+        setError(message, "Message is Required");
+    } else if (messageValue.length < 10) {
+        setError(message, "Message must be at least 10 characters");
+    } else {
+        setSuccess(message);
+    }
+}
