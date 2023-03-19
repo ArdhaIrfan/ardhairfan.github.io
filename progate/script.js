a1f9610ad034466cbb2cbaaefa0d1eb6a164c780
ArdhaIrfan <ardhairfan17@gmail.com>
2023-03-19 13:40:10 +0700

    Add Accordion

--- progate/script.js
+++ progate/script.js
@@ -129,3 +129,23 @@ const validateInputs = () => {
         setSuccess(message);
     }
 }
+
+// ================================= FAQ =================================
+
+const faqs = document.querySelectorAll('.faq-item');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        const icon = faq.querySelector('.fa-solid');
+
+        faq.classList.toggle('active');
+
+        if (icon.classList == 'fa-solid fa-angle-down') {
+            icon.classList.remove('fa-angle-down');
+            icon.classList.add('fa-angle-up');
+        } else {
+            icon.classList.remove('fa-angle-up');
+            icon.classList.add('fa-angle-down');
+        }
+    })
+})
